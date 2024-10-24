51a4e14823c18706b88779a759e14fbab78c2386
Alan-Nixon <alannixon2520@gmail.com>
2024-10-24 23:35:25 +0530

    local commit

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,22 +31,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.generateName = void 0;
 const util_1 = require("util");
-const fs_1 = __importDefault(require("fs"));
-const path_1 = require("path");
-const readFileAsync = (0, util_1.promisify)(fs_1.default.readFile);
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
+const readFileAsync = (0, util_1.promisify)(fs.readFile);
 const generateName = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const fNameIndex = Math.floor(Math.random() * 5008);
         const lNameIndex = Math.floor(Math.random() * 150);
-        const path = (0, path_1.resolve)(__dirname, "../names.txt");
-        console.log(path);
-        const data = yield readFileAsync(path, 'utf-8');
+        const pathForName = path.resolve(__dirname, "../names.txt");
+        const data = yield readFileAsync(pathForName, 'utf-8');
         if (data) {
             const Data = JSON.parse(data);
             return Data[0][fNameIndex] + "_" + Data[1][lNameIndex] + "_" + Math.floor(Math.random() * 10000);
@@ -34,4 +53,3 @@ const generateName = () => __awaiter(void 0, void 0, void 0, function* () {
     }
 });
 exports.generateName = generateName;
-(0, exports.generateName)();
--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import { promisify } from 'util';
-import fs from 'fs'
-import { resolve } from 'path'
+import * as fs from 'fs';
+import * as path from 'path';
+
 const readFileAsync = promisify(fs.readFile);
 
 export const generateName = async () => {
     try {
         const fNameIndex = Math.floor(Math.random() * 5008);
         const lNameIndex = Math.floor(Math.random() * 150);
-        const path = resolve(__dirname,"../names.txt")
-        const data = await readFileAsync(path, 'utf-8');
+        const pathForName = path.resolve(__dirname,"../names.txt")
+        const data = await readFileAsync(pathForName, 'utf-8');
         if (data) {
             const Data = JSON.parse(data);
             return Data[0][fNameIndex] + "_" + Data[1][lNameIndex] + "_" + Math.floor(Math.random() * 10000);
@@ -17,5 +18,3 @@ export const generateName = async () => {
         console.error('Error reading file:', err);
     }
 }
-
-generateName()
